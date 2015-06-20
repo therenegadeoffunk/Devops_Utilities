0ad2cf15e46246f7caa8cf460afff79e1f9d62eb
JonBethune <jbethune@timehop.com>
2015-06-20 11:10:11 -0400

    Skeleton role and box setup script
    Just a start. I want a set of Chef-related tools
    as well as some BASH scripts.

---
+++ developer_setup.sh
@@ -0,0 +1,23 @@
+# This is a basic box setup script for ubuntu development environments
+# It can be used for linux laptops or cloud-based development environments
+
+# Install needed software
+
+sudo apt-get install vim
+sudo apt-get install virtualbox
+sudo apt-get install docker.io
+sudo apt-get install vagrant
+sudo apt-get install rvm
+sudo apt-get install chef
+
+# Create directory structure
+
+mkdir ~/workspace
+
+# TODO: Clone projects if necessary
+
+# TODO: Setup ssh
+
+# TODO: Security configuration
+
+# TODO: Set up swap if necessary
